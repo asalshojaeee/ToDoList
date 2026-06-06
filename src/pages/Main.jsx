41f083d9c195191977a477dfacffc96f3654b036
Asal Shojaee <asal.shojaee.15@gmail.com>
2026-06-06 08:30:38 -0700

    style(Main):Change background  color

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -2,8 +2,8 @@ import { useEffect, useState } from "react";
 
 import { MdDeleteOutline } from "react-icons/md";
 
-
-
+import img from '../assets/images/icons8-to-do-list-96.png'
+import '../assets/styles/Main.css'
 const Main = () => {
 
     const [inptValue, setInptValue] = useState("");
@@ -39,16 +39,23 @@ const Main = () => {
 
 
         <>
-            <section className="mx-auto w-200 h-auto mt-36 bg-green-200 p-5 flex row justify-center items-center rounded-md">
 
-                {/* <img src="icons8-to-do-list-96" alt="" /> */}
+            <div className="flex justify-center m-5 gap-5 items-center mt-24">
+                <img src={img} alt="" className="w-15 h-15"/>
+                <p className="text-orange-400 font-bold text-xl">ToDoList</p>
+
+            </div>
+
+
+            <section className="mx-auto w-200 h-auto  bg-green-200 p-5 flex col justify-center items-center rounded-md">
+
 
                 <div className="">
                     <input
                         value={inptValue}
 
                         onChange={handleChange}
-                        type="text" className="border-2 rounded-md outline-0 text-gray-500 p-3 w-100 border-green-600" placeholder="Type your task..." />
+                        type="text" className="border-2 focus:border-3 transition-all rounded-md outline-0 text-gray-500 p-3 w-100 border-green-600" placeholder="Type your task..." />
 
                     <button
                         onClick={handleClick}
@@ -56,11 +63,11 @@ const Main = () => {
 
                     {task.map((el, index) => {
                         return (
-                            <div key={index} className="flex m-5 h-25  rounded-md justify-between p-5 items-center bg-green-300 text-white">
+                            <div key={index} className="flex m-5 h-25  rounded-md justify-between p-5 items-center style text-white">
                                 <p className="font-medium text-lg">{el}</p>
                                 <MdDeleteOutline
                                     onClick={() => handleDelete(index)}
-                                    size={25} className="text-red-500 cursor-pointer hover:text-red-600 transition-all" />
+                                    size={25} className="text-red-600 cursor-pointer hover:text-red-500 transition-all hover:-translate-y-1" />
                             </div>
                         )
 
